480b75faace9a6646d79a5719ad7e6521ac5bfdb
Syaipuddin <muda3332@gmail.com>
2023-09-02 23:51:05 +0700

    Add First odoo module

---
+++ dev/om_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : 'Hospital Management System',
+    'author' : 'Syaipuddin',
+    'website' : 'www.odoo.com',
+    'summary' : 'Trying odoo Python custom modules',
+    'data' : [
+        'security/ir.model.access.csv',
+        'views\menu.xml',
+        'views\patient.xml'
+    ]
+}
